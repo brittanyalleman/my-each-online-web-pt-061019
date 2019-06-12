18bd41bdf202801dfcf29c8233c268f3e0f00551
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-12 17:58:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,5 +4,6 @@ def my_each(array)
  yield(array[i])
     i += 1
  end
+ return array
 end
 #You'll be building out the content of the `#my_each` method. This method should accept an argument of an array and use the `while` loop to iterate over each member of that array, yielding each element contained in the array to a block.
