3e69437c36ccded9fbdb6573081708f25c36bb0f
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-12 17:51:34 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,7 @@
 def my_each(array)
- while(array) do |i
+  i = 0
+ while i < array.length
+    |i + 1|
 
 end
 #You'll be building out the content of the `#my_each` method. This method should accept an argument of an array and use the `while` loop to iterate over each member of that array, yielding each element contained in the array to a block.
